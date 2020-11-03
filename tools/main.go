9e353e26371ed279504e49439d7914b54b7692f0
Scott Xu <fgsoap@gmail.com>
2020-11-03 21:26:55 +0800

    feat: try flag

---
+++ tools/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	var name string
+	flag.StringVar(&name, "name", "Go tour", "help info")
+	flag.StringVar(&name, "n", "Go tour", "help info")
+	flag.Parse()
+
+	log.Printf("name: %s", name)
+}
