daeddd840f351f8caa761ecdd093c9e5f39ce9a7
Scott Xu <fgsoap@gmail.com>
2020-11-04 22:32:37 +0800

    feat: test sub commands

--- tools/main.go
+++ tools/main.go
@@ -5,11 +5,25 @@ import (
 	"log"
 )
 
+var name string
+
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "Go tour", "help info")
-	flag.StringVar(&name, "n", "Go tour", "help info")
 	flag.Parse()
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goCmd.StringVar(&name, "name", "go language", "help")
+	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+	phpCmd.StringVar(&name, "n", "php language", "help")
+	args := flag.Args()
+	if len(args) < 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		_ = phpCmd.Parse(args[1:])
+	}
 
 	log.Printf("name: %s", name)
 }
